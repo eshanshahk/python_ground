99aae46627423fef19a6e09775ea3c3b3948885b
ESHAN SHAH K <eshanshahk123@gmail.com>
2025-06-24 20:21:07 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,3 +1,4 @@
 print("Hey Guys!!")# to print anythin in python
 print("""Hello
               world!!!!""") #""" used for printing multiple lines
+print(f"There are {365/7:.0f} weeks in a year") #formated string and decimal places
