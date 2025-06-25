876f7e72ebc706772a75e122e16cc60301739f03
ESHAN SHAH K <eshanshahk123@gmail.com>
2025-06-25 17:50:39 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -4,3 +4,8 @@ print("""Hello
 print(f"There are {365/7:.0f} weeks in a year") #formated string and decimal places
 from helper_functions import print_llm_response #use this to do llm prompting using variables
 print_llm_response("What is the capital of France?") #eg
+hey_list=["a","b","c"] #list datatype
+hey_list.append("d") #to add onw element
+hey_list.remove("a") #to remove
+for task in list_of_task:
+  print task() #for loop 
